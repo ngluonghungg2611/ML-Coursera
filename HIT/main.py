bd3f90c66f27afcf34a3673583c72a7b097efe5a
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2021-10-03 00:54:42 +0700

    x

---
+++ HIT/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import cv2 #opencv-python
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Khởi tạo mảng
+arr = np.array([1,2,3,4])
+
+
+#   Khởi tạo mảng 2 chiều
+array2 = np.array([[1,2,3],[3,5,8]])
+array2_2 = np.array([(1,2,3), (5,1,3)])
+print(array2)
+print(array2_2)
+
