da5f024fb36e5aace5fffcf413b3439b3091ef20
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2021-10-22 15:52:47 +0700

    Random Forest Regression

--- Regression/Random_Forest_REgression/Random_Forest_Regression.py
+++ Regression/Random_Forest_REgression/Random_Forest_Regression.py
@@ -1,4 +1,5 @@
 import numpy
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -18,4 +19,30 @@ RANDOM FOREST REGRESSION
         Ví dụ có 500 Tree và nhận được 500 giá trị predict -> Giá trị predict cuối cùng là trung bình cộng của 500 giá trị
         dự đoán
             -> Điều này cải thiện được độ chính xác vì đang lấy giá trị trung bình của nhiều dự đoán
+'''
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(X,y)
+pred = regressor.predict([[6.5]])
+print(pred)
+# plt.scatter(X,y, color = 'red')
+# plt.plot(X, regressor.predict(X), color = 'blue')
+# plt.title('Random Forest REgression')
+# plt.xlabel('Position level')
+# plt.ylabel('Salary')
+# plt.show()
+
+X_grid = np.arange(start=min(X), stop=max(X), step = 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'r')
+plt.plot(X_grid, regressor.predict(X_grid), color='b')
+plt.title('RAndom Forest Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+'''
+    Được chia thành nhiều bước hơn vì có nhiều Decision Tree tính toán trong các khoảng
 '''
