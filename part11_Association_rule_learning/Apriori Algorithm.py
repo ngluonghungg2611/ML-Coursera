283895c44e5d7722bcf272ff28d74c44d0bfefbb
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2021-10-25 20:31:37 +0700

    Apiori Algorithm Coding with optimisation sales in market

---
+++ part11_Association_rule_learning/Apriori Algorithm.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+'''
+    - Mục đích của bài toán: Tối ưu hóa doanh số bán hàng trong của hàng tạp hóa
+    - Sử dụng Association rule learning để biết chính xác vị trí đặt sản phẩm trong của hàng
+'''
