0965c77ac6d55ea8986b45212a951100dd8cd339
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2021-10-26 22:30:22 +0700

    Apriori Algorithm coding

--- part11_Association_rule_learning/Apriori Algorithm.py
+++ part11_Association_rule_learning/Apriori Algorithm.py
@@ -5,3 +5,20 @@ import matplotlib.pyplot as plt
     - Mục đích của bài toán: Tối ưu hóa doanh số bán hàng trong của hàng tạp hóa
     - Sử dụng Association rule learning để biết chính xác vị trí đặt sản phẩm trong của hàng
 '''
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+# print(dataset)
+transactions = []
+for i in range(0,7501):
+    transactions.append( [ str(dataset.values[i,j]) for j in range(0,20) ] )
+
+print(transactions)
+
+
+
+
+
+
+
+
+
+''
