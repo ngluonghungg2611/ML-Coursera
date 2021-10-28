1a19e34cba3b839371817013ef8087f9520ee713
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2021-10-28 16:50:05 +0700

    OCR

---
+++ part12_OCR/1_ImagePreprocessing/Image_Preprocessing.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+#   Divide by max
+def devide_by_max(img):
+    return img/255
+
+#   Mix-max scaling
+def min_max_scaking(img):
+     img_min = np.min(img)
+     img_max = np.max(img)
+     return (img - img_min) / (img_max - img_min)
+
+# Standardization
+def standardization(img):
+     mu = img.mean()
+     std = img.std()
+     return (img - mu) / std
