515b3911fc487571e0d5d06425e6e7bf32441842
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2021-10-29 19:40:22 +0700

    Apriori Algorithm

--- part11_Association_rule_learning/Apriori Algorithm.py
+++ part11_Association_rule_learning/Apriori Algorithm.py
@@ -11,14 +11,5 @@ transactions = []
 for i in range(0,7501):
     transactions.append( [ str(dataset.values[i,j]) for j in range(0,20) ] )
 
-print(transactions)
-
-
-
-
-
-
-
-
-
-''
+from apyori import apriori
+rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2)
