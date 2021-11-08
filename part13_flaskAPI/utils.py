dab5304d1ba93bb4d8cfb26439e7cea8f8270479
ngluonghungg2611 <ngluonghungg2611@gmail.com>
2021-11-08 19:39:44 +0700

    utils

---
+++ part13_flaskAPI/utils.py
@@ -0,0 +1,41 @@
+'''
+    File chứa các hàm tiện ích được sự dùng nhiều module trong project
+'''
+from ctypes import resize
+import re
+from tensorflow.python.keras.backend import conv2d_transpose
+import hyper as hp
+from tensorflow.keras.applications import MobileNet, ResNet50
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+import json
+
+def _load_model():
+    model = ResNet50(weights='imagenet')
+    print("Load model completed")
+    return model
+
+def _preprocess_image(img, shape):
+    img_rz = img.resize(shape)
+    img_rz = img_to_array(img_rz)
+    img_rz = np.expand_dims(img_rz, axis=0)
+    return img_rz
+
+
+class NumpyEncoder(json.JSONEncoder):
+    '''
+        Encoding numpy into json
+    '''
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, np.int32):
+            return int(obj)
+        if isinstance(obj, np.int64):
+            return int(obj)
+        if isinstance(obj, np.float32):
+            return float(obj)
+        if isinstance(obj, np.float64):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+
